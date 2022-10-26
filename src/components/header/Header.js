0e646dab953a2cf578643930296440e663429cde
Shameer-U <shameerushahul@gmail.com>
2022-10-26 21:39:03 +0530

    basic of search page completed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,20 +6,19 @@ import LANGUAGES from "../../constants/Languages";
 import IMAGES from '../../constants/Images'
 
 const Header = () => {
-    const [term, setTerm] = useState('');
-    const [searchResults, setsearchResults] = useState({});
-    const [items, setItems] = useState([]);
+    const [term, setTerm] = useState('')
+    const [searchResults, setsearchResults] = useState({})
     const [pagination, setPagination] = useState({
         previous:0,
         current : 0,
         next : 0,
-        totalpages: 0
+        totalPages: 0
     })
 
 
     const submitHandler = (e) => {
-        e.preventDefault();
-        e.stopPropagation();
+        e.preventDefault()
+        e.stopPropagation()
         if(term === '') {
             return alert('Please enter search term')
         }
@@ -38,14 +37,16 @@ const Header = () => {
             previous: Number(response.data.page) - 1,
             current : response.data.page,
             next : Number(response.data.page) + 1,
-            totalpages: response.data.total_pages
+            totalPages: response.data.total_pages
         })
     }
 
-    const getLanguage = (language_iso) => LANGUAGES.find((language) => language.iso_639_1 === language_iso);
+    const getLanguage = (language_iso) => { 
+        return LANGUAGES.find((language) => language.iso_639_1 === language_iso)
+    }
 
-    const changePage = (goTo) => {
-        search(term, goTo)
+    const changePage = (page) => {
+        search(term, page)
     }
 
 
@@ -59,47 +60,46 @@ const Header = () => {
                 </form>
             </div>
         </div> 
-        {pagination.current &&
         <div>
-                    <button onClick={() => changePage(pagination.previous)}>
-                       previous
-                    </button>
-                    <span>
-                       {pagination.current}
-                    </span>
-                    <button onClick={() => changePage(pagination.next)}>
-                      next
-                    </button>
-        </div>
-        }
-        <div className="card-container">
-            {searchResults.results !== undefined ? ( searchResults.results.map((item, index) => {
-                
-                return <div className="card-item">
-                            <div className="card-inner">
-                                <div className="card-top">
-                                    <img className="card-poster" src={`${TMDB_IMAGE_BASE_URL}/original${item.poster_path}`} alt='' />
-                                    <div className='card-rating'>
-                                        <img src={IMAGES.IMDB} />
-                                        <span >{item.vote_average}</span>
+            <div className="card-container">
+                {searchResults.results !== undefined ? ( searchResults.results.map((item, index) => {
+                    
+                    return <div className="card-item">
+                                <div className="card-inner">
+                                    <div className="card-top">
+                                        <img className="card-poster" src={`${TMDB_IMAGE_BASE_URL}/original${item.poster_path}`} alt='' />
+                                        <div className='card-rating'>
+                                            <img src={IMAGES.IMDB} />
+                                            <span >{item.vote_average}</span>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="card-bottom">
-                                    <div className="movie-title">
-                                         <h4>{(item.title.length > 40) ? item.title.substring(0, 40) + '...' : item.title}</h4>
-                                    </div>
-                                    <div className="movie-subtitle">
-                                        <p>{getLanguage(item.original_language).english_name}</p>
-                                        <span><i className='fa fa-heart'></i>{item.vote_count}</span>
+                                    <div className="card-bottom">
+                                        <div className="movie-title">
+                                            <h4>{(item.title.length > 40) ? item.title.substring(0, 40) + '...' : item.title}</h4>
+                                        </div>
+                                        <div className="movie-subtitle">
+                                            <p>{getLanguage(item.original_language).english_name}</p>
+                                            <span><i className='fa fa-heart'></i>{item.vote_count}</span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-            })) : <div className="shows-error">
-                    <h3>No results found</h3>
-                  </div>
-            }
+                })) : 
+                <div className="no-result">
+                        <h3>No results found</h3>
+                </div>
+                }
             </div>
+            {pagination.current !==0 &&
+                <div className="pagination-container">
+                    <div className="pagination">
+                        <button onClick={() => changePage(pagination.previous)}>previous</button>
+                        <span>{pagination.current}</span>
+                        <button onClick={() => changePage(pagination.next)}>next</button>
+                    </div>
+                </div>
+            }
+        </div>
         </>
     )
 }
